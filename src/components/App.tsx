1195681061f2bba347779fd5ab1e372f264c22af
John Reilly <johnny_reilly@hotmail.com>
2015-10-30 06:47:55 +0000

    Started putting together repo based on cut down version of gsww

---
+++ src/components/App.tsx
@@ -0,0 +1,74 @@
+import * as React from 'react/addons';
+import ModuleStore from '../stores/ModuleStore';
+import ModuleSelector from './ModuleSelector';
+import * as ModuleActions from '../actions/ModuleActions';
+import ModulesState from '../types/ModuleState';
+import { determineRequiredCldrData, determineRequiredCldrGlobalizeFiles } from '../../../index';
+
+interface State {
+  modulesState: ModulesState
+}
+
+class App extends React.Component<any, State> {
+  constructor(props) {
+    super(props);
+    this._onChange = this._onChange.bind(this);
+    this._handleSelectionChange = this._handleSelectionChange.bind(this);
+    this.state = this._getStateFromStores();
+  }
+
+  componentWillMount() {
+    ModuleStore.addChangeListener(this._onChange);
+  }
+
+  componentWillUnmount() {
+    ModuleStore.removeChangeListener(this._onChange);
+  }
+
+  render() {
+    const { modulesState } = this.state;
+    const optionsSelected = Object.assign({}, ...Object.keys(modulesState).map(mod => ({ [`${mod}`]: modulesState[mod].isSelected })));
+    const requiredCldrJson = determineRequiredCldrData(optionsSelected).map(file => <li key={ file }>{ file }</li>);
+    const requiredCldrGlobalizeFiles = determineRequiredCldrGlobalizeFiles(optionsSelected).map(file => <li key={ file }>{ file }</li>);
+
+    return (
+      <div className="container-fluid">
+        <h1>Welcome to Globalize &middot; So What&#39;cha Want</h1>
+
+        <p>Tell me what <a href="https://github.com/jquery/globalize">Globalize</a> modules you want to use, I&#39;ll tell you what you need and in the order you need it.</p>
+
+        <ModuleSelector modulesState={ modulesState } handleSelectionChange={ this._handleSelectionChange } />
+
+        <div className="row">
+          <div className="col-md-6">
+            <h4>Required CLDR / Globalize files</h4>
+            <ul>
+              { requiredCldrGlobalizeFiles }
+            </ul>
+          </div>
+
+          <div className="col-md-6">
+            <h4>Required CLDR JSON</h4>
+            <ul>
+              { requiredCldrJson }
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  _onChange() {
+    this.setState(this._getStateFromStores());
+  }
+
+  _handleSelectionChange(moduleName) {
+    ModuleActions.moduleChanged(moduleName);
+  }
+
+  _getStateFromStores() {
+    return ModuleStore.getState();
+  }
+}
+
+export default App;
---
+++ src/stores/ModuleStore.ts
@@ -0,0 +1,51 @@
+import FluxStore from './FluxStore';
+import ModuleActionTypes from '../constants/action-types/ModuleActionTypes';
+import AppDispatcher from '../dispatcher/AppDispatcher';
+import ModulesState from '../types/ModuleState';
+let modulesState = _getNewState();
+
+class ModuleStore extends FluxStore {
+  constructor() {
+    super();
+  }
+
+  getState() {
+    return { modulesState };
+  }
+}
+
+const moduleStoreInstance = new ModuleStore();
+
+moduleStoreInstance.dispatchToken = AppDispatcher.register(_dispatcherHandler);
+
+// TODO: Make this conditional so it only happens in test? - look at envify
+moduleStoreInstance._dispatcherHandler = _dispatcherHandler;
+moduleStoreInstance._cleanState = _cleanState;
+
+function _getNewState(): ModulesState {
+  return {
+    currency     : { isSelected: false, description: 'Currency module provides currency formatting and parsing' },
+    date         : { isSelected: true,  description: 'Date module provides date formatting and parsing' },
+    message      : { isSelected: false, description: 'Message module provides ICU message format support' },
+    number       : { isSelected: true,  description: 'Number module provides number formatting and parsing' },
+    plural       : { isSelected: false, description: 'Plural module provides pluralization support' },
+    relativeTime : { isSelected: false, description: 'Relative time module provides relative time formatting support' },
+    unit         : { isSelected: false, description: 'Unit module provides unit formatting support' }
+  };
+}
+
+function _cleanState() {
+  modulesState = _getNewState();
+}
+
+function _dispatcherHandler(action) {
+  switch(action.type) {
+    case ModuleActionTypes.MODULE_CHANGED:
+      const { globModule } = action;
+      modulesState[globModule].isSelected = !modulesState[globModule].isSelected;
+      moduleStoreInstance.emitChange();
+      break;
+  }
+}
+
+export default moduleStoreInstance;
